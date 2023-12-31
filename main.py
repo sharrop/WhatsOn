60946e458f8e7a6b4e850219e8edf6cf97a006bd
sharrop <harrop.home@gmail.com>
2023-12-31 17:09:03 +0000

    Bug fix: Single date event format

--- main.py
+++ main.py
@@ -29,12 +29,41 @@ while listing_page:
         try:
             item = listing.article
 #            div = item.div
-            subject = item.find('h2', attrs = {'class': 'listing__item__title'}).text
+            subject_node = item.find('h2', attrs = {'class': 'listing__item__title'})
+            if subject_node:
+                subject = item.find('h2', attrs = {'class': 'listing__item__title'}).text
+            else:
+                subject = 'Not Stated'
+
             event_url = item.div.a.get("href")
-            event_date = item.find('p', attrs = {'class': 'listing__item__date'}).text.strip()
-            venue = item.find('p', attrs = {'class': 'listing__item__venue'})
-            if (venue):
-                event_location = venue.text.strip()
+            event_date_node = item.find('p', attrs = {'class': 'listing__item__date'})
+            if event_date_node:
+                event_date = event_date_node.text.strip()
+                # Convert the event date to a date object from: "Friday 24th November 2023"
+                # TODO: Need to deal with: "Wednesday 13th December 2023 — Tuesday 2nd January 2024"
+                # Strip 'th', 'rd', 'nd', 'st'from the date string, and convert to a date object.
+                event_date = event_date.replace('th ', ' ')
+                event_date = event_date.replace('st ', ' ') #TODO: Kills 'August'
+                event_date = event_date.replace('rd ', ' ')
+                event_date = event_date.replace('nd ', ' ')
+                if '—' in event_date:
+                    date_text = event_date.split('—')
+                    start = date_text[0].strip()
+                    end = date_text[1].strip()
+                    # Need to put these in the format: 05/30/2020
+                    captured_start_date = datetime.strptime(start, '%A %d %B %Y').date()
+                    start_date = captured_start_date.strftime('%m/%d/%Y')
+                    captured_end_date = datetime.strptime(end, '%A %d %B %Y').date()
+                    end_date = captured_end_date.strftime('%m/%d/%Y')
+                else:
+                    captured_date = str(datetime.strptime(event_date, '%A %d %B %Y').date())
+                    start_date = end_date = captured_end_date.strftime('%m/%d/%Y')
+            else:
+                raise Exception(f'No event date specified for [{subject}] -- skipping')
+
+            venue_node = item.find('p', attrs = {'class': 'listing__item__venue'})
+            if (venue_node):
+                event_location = venue_node.text.strip()
             else:
                 event_location = 'Not Stated'
 
@@ -42,32 +71,15 @@ while listing_page:
             driver.get(event_url);
             event_soup = BeautifulSoup(driver.page_source, 'html.parser')
             price = event_soup.find('h2', attrs = {'class': 'event__meta__tickets'})
-            event_time = event_soup.find('a', attrs = {'data-tooltip-content': '#tooltip_content__1'}).text.strip()
-            # Remove all text beyond the am or pm from the time string
-            event_time = re.sub(r'm.*$', 'm', event_time, flags=re.S)
-            event_time = event_time.replace('am', ' AM')
-            event_time = event_time.replace('pm', ' PM')
-
-            # Convert the event date to a date object from: "Friday 24th November 2023"
-            # TODO: Need to deal with: "Wednesday 13th December 2023 — Tuesday 2nd January 2024"
-            # Strip 'th', 'rd', 'nd', 'st'from the date string, and convert to a date object.
-            event_date = event_date.replace('th ', ' ')
-            event_date = event_date.replace('st ', ' ')
-            event_date = event_date.replace('rd ', ' ')
-            event_date = event_date.replace('nd ', ' ')
-            if '—' in event_date:
-                date_text = event_date.split('—')
-                start = date_text[0].strip()
-                end = date_text[1].strip()
-                # Need to put these in the format: 05/30/2020
-                captured_start_date = datetime.strptime(start, '%A %d %B %Y').date()
-                start_date = captured_start_date.strftime('%m/%d/%Y')
-                captured_end_date = datetime.strptime(end, '%A %d %B %Y').date()
-                end_date = captured_end_date.strftime('%m/%d/%Y')
+            event_time_node = event_soup.find('a', attrs = {'data-tooltip-content': '#tooltip_content__1'})
+            if (event_time_node):
+                event_time = event_time_node.text.strip()
+                # Remove all text beyond the am or pm from the time string
+                event_time = re.sub(r'm.*$', 'm', event_time, flags=re.S)
+                event_time = event_time.replace('am', ' AM')
+                event_time = event_time.replace('pm', ' PM')
             else:
-                captured_date = str(datetime.strptime(event_date, '%A %d %B %Y').date())
-                start_date = captured_date
-                end_date = captured_date
+                event_time = 'Not Stated'
 
             price_text = price.text.strip()
             description = item.find('div', attrs = {'class': 'listing__item__summary'}).p.text
